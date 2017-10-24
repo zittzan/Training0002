7e1ac8027f79e0481f03867690c0e6bfa04bae9e
中谷 亘 <shadow75wx@gmail.com>
2017-10-24 12:13:13 +0900

    ロケットみたいに飛んでくスクリプトやで

---
+++ Assets/Rocket.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+	public Rigidbody rBody;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rBody.AddForce (Vector3.up * 10f, ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
